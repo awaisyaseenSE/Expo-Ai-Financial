414da14e3e0fd15ac2ba26d9952ec1d8aa03e3f5
awaisyaseenSE <148967337+awaisyaseenSE@users.noreply.github.com>
2024-07-03 12:47:16 +0500

    Remove sensitive Firebase configuration from repository

--- app/constants/constants.js
+++
@@ -1,8 +0,0 @@
-export default {
-  api_key: "AIzaSyA2VYOxW2fvH5Y5L2Bl5gA-_Tv1Hm7gG60",
-  auth_domain: "expo-54915.firebaseapp.com",
-  project_id: "expo-54915",
-  storage_backet: "expo-54915.appspot.com",
-  message_sender_id: "1064406560723",
-  app_id: "1:1064406560723:web:11d39134f95410a4b00926",
-};
